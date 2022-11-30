20cb8bc5055181dfd7a5d6ca78c3cf29d8e5a91c
Kahnurd <hetti031@morris.umn.edu>
2022-11-29 21:38:20 -0600

    Added comments

--- Huffingman.java
+++ Huffingman.java
@@ -12,9 +12,12 @@ public class Huffingman {
     public ArrayList<Node> nodeArray = new ArrayList<Node>();;
     
 /**
- * 
- * @param input
- * @param freqOrBin
+ * This function is responsible for reading the input file and creating a node array that gets used
+ * in the function createTree and createBinaryTree. After creating the ArrayList of nodes, it calls
+ * createTree() or createBinaryTree() depending on the boolean value.
+ * @param input Takes the name of a file
+ * @param freqOrBin Takes a boolean value, true means the input has the frequencies of the letters
+ * false means the input has the binary representations.
  */
     public Huffingman(String input, Boolean freqOrBin){
         try{
@@ -38,6 +41,14 @@ public class Huffingman {
         }
     }
 
+    /**
+     * createTree has no input, but uses the ArrayList of nodes made by the Huffingman constructor.
+     * It takes the ArrayList and creates a PriorityQueue of the nodes. Then it loops through taking
+     * the two letters with the lowest frequencies (first two in queue) and combines them by making a parent
+     * node with a frequency equal to the sum of the two given frequencies, then it makes the two given nodes children
+     * of the new node. It does this over and over again until it creates a node with a frequency value of 100%, then
+     * it makes that node the root node. Then it sends the root node to the HuffingAlgorithm method
+     */
     public void createTree(){
         Node first;
         Node second;
@@ -81,6 +92,14 @@ public class Huffingman {
 
     }
 
+    /**
+     * This function takes the root node of the tree created by createTree(). It recursively
+     * explores the tree adding a string value to each node's binary representation. It adds the string
+     * 0 to every left child's binary representation and 1 to every right child's binary representation.
+     * This hits every node in the tree and accurately gives each letter their binary representation according to
+     * the Huffing Algorithm
+     * @param node
+     */
     public static void HuffingAlgorithm(Node node){
         if(node == null) return;
         if(node.leftChild !=null) node.leftChild.binary = node.binary+"0";
@@ -89,6 +108,12 @@ public class Huffingman {
         HuffingAlgorithm(node.rightChild);
     }
 
+    /**
+     * This function takes the name of the output file and outputs each letter with its binary representation.
+     * It throws the ArrayList of nodes into a priority queue that organizes it by length of binary representation.
+     * Then it writes each node letter next to its binary represention in that order.
+     * @param outFile Takes the name of the output file
+     */
     public void outputList(String outFile){
         try{
         FileWriter writer= new FileWriter(outFile);
@@ -105,6 +130,16 @@ public class Huffingman {
     } catch (IOException e) { System.out.println("IOException in the outputList method");}
     }
 
+    /**
+     * This function takes the encrypted text and turns it into the original characters.
+     * It starts at the root node, and iterates through by following the binary given.
+     * If the current character is a 1 it goes to the right and if it is a 0 it goes to the left.
+     * It does this until it hits a leaf node (a node that has a character value). Once it finds one, it adds
+     * it to the result string and starts back at the root continuing the process until it runs out of characters in the given string.
+     * At the end it outputs the accumulated result string.
+     * @param cryptText Takes text encrypted by the same Huffing tree as this instance
+     * @return Returns the decrypted text with no spaces or puncuation.
+     */
     public String decrypt(String cryptText){
         char[] text = cryptText.toCharArray();
         Node currNode = this.root;
@@ -124,6 +159,16 @@ public class Huffingman {
         return result;
     }
 
+    /**
+     * This function takes some plaintext that is assumed to have the characters in the Huffing Tree and
+     * converts it to their binary represntation without any spaces. Every 70 characters it will also
+     * add a \n to make it more readable. The function ends when it runs out of characters to convert.
+     * 
+     * @param plainText Takes some plaintext without any spaces or special characters.
+     * If a character in the plaintext doesn't exist in the Huffing Tree it won't print anything
+     * for that character
+     * @return Returns the encrypted text in 1's and 0's
+     */
     public String encrypt(String plainText){
         String result = "";
         int counter = 0;
@@ -140,6 +185,15 @@ public class Huffingman {
         return result;
     }
 
+    /**
+     * This function takes information from the encrypt() method, like the letter to be encrypted
+     * and the root node of the Huffing Tree. It visits every node in the tree and adds all the returned
+     * strings together. The only time the function returns an actual string is when it finds the node with
+     * the matching character. So at the end it should only have the correct binary. 
+     * @param currentChar Takes the character that is to be encrypted
+     * @param node Takes the root node of the tree used to encrypt the text
+     * @return returns the binary representation of the given character
+     */
     private String encryptHelper(char currentChar, Node node){
         if(node != null) {
         if(node.letter == currentChar) return node.binary;
@@ -147,6 +201,12 @@ public class Huffingman {
         } else return "";
     }
 
+    /**
+     * This function finds the average binary length of the current tree in this instance. 
+     * It goes through every node in the ArrayList of nodes and sums their binary length multiplied
+     * by their frequency. 
+     * @return returns the average binary length
+     */
     private String averageLength(){
         double total = 0;
         for (Node node : nodeArray) {
